f175aeba00a38ea268f07350159929452a82a969
AlejoG10-uniandes <alejogonza99@hotmail.com>
2023-09-06 23:01:04 -0500

    Fix build bug

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -7,24 +7,28 @@ interface IParams {
 }
 
 export async function DELETE(request: Request, { params }: { params: IParams }) {
-    const currentUser = await getCurrentUser();
+    try {
+        const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
+        if (!currentUser) {
+            return NextResponse.error();
+        }
+
+        const { listingId } = params;
+
+        if (!listingId || typeof listingId !== 'string') {
+            throw new Error("Invalid ID")
+        }
+
+        const listing = await prisma.listing.deleteMany({
+            where: {
+                id: listingId,
+                userId: currentUser.id
+            }
+        });
+
+        return NextResponse.json(listing);
+    } catch (error) {
         return NextResponse.error();
     }
-
-    const { listingId } = params;
-
-    if (!listingId || typeof listingId !== 'string') {
-        throw new Error("Invalid ID")
-    }
-
-    const listing = await prisma.listing.deleteMany({
-        where: {
-            id: listingId,
-            userId: currentUser.id
-        }
-    });
-
-    return NextResponse.json(listing);
 }
